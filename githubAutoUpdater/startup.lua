1ff1e757a086158f3932b9a606acf1c74f5a876e
Taylor Hinote <thinote98@hotmail.com>
2025-06-03 13:26:17 -0400

    init

---
+++ githubAutoUpdater/startup.lua
@@ -0,0 +1,20 @@
+local githubRawURL = "https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/main.lua"
+local localFile = "main.lua"
+
+-- Remove old version
+if fs.exists(localFile) then
+    fs.delete(localFile)
+end
+
+-- Attempt to download new version
+local success, err = pcall(function()
+    shell.run("wget", githubRawURL, localFile)
+end)
+
+-- Run the new script if downloaded
+if fs.exists(localFile) then
+    shell.run(localFile)
+else
+    print("Failed to download script.")
+    if err then print(err) end
+end
