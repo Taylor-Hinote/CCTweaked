18eb6ed1132a837f331543831f3e90b9e51ff35b
Taylor Hinote <thinote98@hotmail.com>
2025-06-04 18:52:03 -0400

    fix: enhance mail client to display recent messages and improve user feedback

--- Mailer/Client/mail.lua
+++ Mailer/Client/mail.lua
@@ -85,6 +85,22 @@ local function loadConfig()
     return nil, nil
 end
 
+local messageHistory = {}
+local HISTORY_LIMIT = 10
+
+local function printHeaderAndMessages()
+    term.clear()
+    term.setCursorPos(1, 1)
+    print("[MailClient] Client Version v" .. version)
+    print("[MailClient] Your Computer ID is: " .. os.getComputerID())
+    print("[MailClient] Ready. Type: mail @userName \"Message Here\" or soundTest")
+    print("\n--- Recent Messages ---")
+    for i = math.max(1, #messageHistory - HISTORY_LIMIT + 1), #messageHistory do
+        print(messageHistory[i])
+    end
+    print("----------------------\n")
+end
+
 print("[MailClient] Client Version v" .. version)
 local id, userName = loadConfig()
 if not id or not userName then
@@ -113,19 +129,18 @@ while true do
         if proto == "mail" then
             -- Expecting: senderName|message
             local from, message = msg:match('^([%w_%-]+)%|(.*)$')
+            local displayMsg
             if from and message then
-                print("[MailClient] New mail from @" .. from .. ": " .. message)
+                displayMsg = "[MailClient] New mail from @" .. from .. ": " .. message
             else
-                print("[MailClient] New mail: " .. msg)
+                displayMsg = "[MailClient] New mail: " .. msg
             end
+            table.insert(messageHistory, displayMsg)
+            printHeaderAndMessages()
             playMailSound()
         end
     elseif event == "char" or event == "key" then
-        term.clear()
-        term.setCursorPos(1, 1)
-        print("[MailClient] Client Version v" .. version)
-        print("[MailClient] Your Computer ID is: " .. os.getComputerID())
-        print("[MailClient] Ready. Type: mail @userName \"Message Here\" or soundTest")
+        printHeaderAndMessages()
         write(": ")
         local input = read()
         local name, msg = parseMailCommand(input)
@@ -134,8 +149,11 @@ while true do
             playMailSound()
         elseif name and msg then
             sendMail(name, msg)
+            table.insert(messageHistory, "[MailClient] Sent mail to @" .. name .. ": " .. msg)
+            printHeaderAndMessages()
         elseif input ~= "" then
-            print("[MailClient] Invalid command. Use: mail @userName \"Message Here\" or soundTest")
+            table.insert(messageHistory, "[MailClient] Invalid command. Use: mail @userName \"Message Here\" or soundTest")
+            printHeaderAndMessages()
         end
     end
 end
